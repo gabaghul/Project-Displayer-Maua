80275f22afe33f4ea8f05a5727742408a070e74c
HENRIQUE LADEIRA ROSA POLADIAN <16.00026-9@maua.br>
2019-03-30 10:07:56 -0300

    start react

---
+++ my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './componentes/Card'
+
+const element = <Card nome="Henrique" />
+
+ReactDOM.render( <Card />, document.getElementById('react'))
