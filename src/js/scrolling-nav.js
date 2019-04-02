4620e42fbfd637f1a4120aa6210752f2d9a100ad
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-04-02 19:20:53 -0300

    build complete!

--- src/js/scrolling-nav.js
+++ src/js/scrolling-nav.js
@@ -1,10 +1,4 @@
 $(document).ready( () => {
-//     // Closes responsive menu when a scroll trigger link is clicked
-//     $('.js-scroll-trigger').click(function () {
-//       $('.navbar-collapse').collapse('hide');
-//     });
-
-    // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
       target: '#mainNav',
       offset: 56
