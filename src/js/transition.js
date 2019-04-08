2d5ad238b1ef9d309e7bd6c7f7b4aca7ac07e195
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-04-08 12:18:43 -0300

    visual card definido

--- src/js/transition.js
+++ src/js/transition.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    $('a').click(e => {
+    $('a.js-scroll.trigger').click(e => {
         const link = e.target.getAttribute('href').substr(1)
         document.querySelector(`section[id=${link}]`)
             .scrollIntoView({ block: 'start', behavior: 'smooth' })
