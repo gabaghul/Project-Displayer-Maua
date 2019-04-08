08236cccffa29498f57618e13fde144ef0e8f59b
kikepola <kikepola@gmail.com>
2019-04-08 19:21:31 -0300

    card functions

--- my-app/src/componentes/Card.jsx
+++ my-app/src/componentes/Card.jsx
@@ -1,18 +1,66 @@
 import React, { Component } from 'react'
 import './CardStyle.css'
 import imgTeste from './teste.jpg'
-
+var status_value = 0;
+var i = 0;
 export default class Card extends Component {
+    controllFocus(obj){
+        if((status_value %2) === 0){
+            this.setFocus(obj);
+        }else{
+            this.removeFocus(obj);
+        }
+        status_value = status_value + 1;
+    }
+
+    setFocus(obj){
+        var cards = document.getElementsByClassName("card")
+        for (i = 0; i < cards.length; i++) {
+            if(cards[i].getAttribute("value") !== obj.props.key_ref){
+                cards[i].style.display = "none";
+            }else{
+                if(window.innerWidth > 800){
+                    cards[i].style.width = "100%";
+                }
+                cards[i].getElementsByTagName("button")[0].style.width="30%";
+                cards[i].getElementsByTagName("button")[0].style.borderRadius="30%";
+                cards[i].getElementsByTagName("button")[0].innerHTML = "-"
+                cards[i].getElementsByTagName("button")[0].scrollIntoView();
+            }
+        }
+    }
+    removeFocus(obj){
+        var cards = document.getElementsByClassName("card")
+        for (i = 0; i < cards.length; i++) {
+            if(cards[i].style.display = "none"){
+                cards[i].style.display = "block";
+            }
+        }
+        for (i = 0; i < cards.length; i++) {
+            if(cards[i].getAttribute("value") === obj.props.key_ref){
+                if(window.innerWidth > 800){
+                    cards[i].style.width = "30%";
+                    cards[i].getElementsByClassName("imgContaner")[0].style.margin="";
+                    cards[i].getElementsByClassName("imgContaner")[0].style.width="";
+                    cards[i].getElementsByClassName("imgContaner")[0].style.borderRadius="";
+                }
+                cards[i].getElementsByTagName("button")[0].style.width="90%";
+                cards[i].getElementsByTagName("button")[0].style.borderRadius="20%";
+                cards[i].getElementsByTagName("button")[0].innerHTML = "+"
+            }
+        }
+    }
+
     render(){
         return(
-            <div className="card" >
+            <div className="card" value={this.props.key_ref}>
                 <img src={imgTeste} alt="foto" className="imgContaner"/> 
                 <div className="container">
                     <div className="row_btn">
                         <h4>
                             <b>{this.props.name}</b>
                         </h4>
-                        <button className="button"><span>+</span></button>
+                        <button className="button" value={this.props.key_ref} onClick={() => this.controllFocus(this)}><span>+</span></button>
                     </div>
                     <p>{this.props.text}</p>
                 </div>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,19 +4,17 @@ import Card from './componentes/Card'
 
 var itens = []
 
-const file_1 = <Card class="col-sm" name="Projeto 1" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor nunc a nibh sagittis, et."/>
-const file_2 = <Card class="col-sm" name="Projeto 2" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor nunc a nibh sagittis, et."/>
+const file_1 = <Card class="col-sm" name="Projeto 1" key_ref="1" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor nunc a nibh sagittis, et."/>
+const file_2 = <Card class="col-sm" name="Projeto 2" key_ref="2" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor nunc a nibh sagittis, et."/>
+const file_3 = <Card class="col-sm" name="Projeto 3" key_ref="3" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor nunc a nibh sagittis, et."/>
+const file_4 = <Card class="col-sm" name="Projeto 4" key_ref="4" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor nunc a nibh sagittis, et."/>
+
 
 itens.push(file_1)
-itens.push(file_1)
-itens.push(file_1)
-itens.push(file_1)
-itens.push(file_1)
-itens.push(file_1)
-itens.push(file_1)
-itens.push(file_1)
-itens.push(file_1)
 itens.push(file_2)
+itens.push(file_3)
+itens.push(file_4)
+
 
 
 ReactDOM.render(itens, document.getElementById('react'))
