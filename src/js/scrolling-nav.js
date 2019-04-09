cccc6a662b8482ff48f5adc9a0b735e5d844b8b5
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-04-09 18:00:18 -0300

    hr card e scroll

--- src/js/scrolling-nav.js
+++ src/js/scrolling-nav.js
@@ -1,8 +1,13 @@
-$(document).ready( () => {
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 56
-    });
+$(document).ready(() => {
+  $('body').scrollspy({
+    target: '#mainNav'
+  });
 
-  }
-)
+  $('a.js-scroll-trigger').click(e => {
+    e.preventDefault()
+    const link = e.target.getAttribute('href').substr(1)
+    console.log(link, typeof link)
+    document.querySelector(`section[id=${link}]`)
+      .scrollIntoView({ block: 'start', behavior: 'smooth' })
+  });
+})
--- src/js/transition.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-    $('a.js-scroll.trigger').click(e => {
-        const link = e.target.getAttribute('href').substr(1)
-        document.querySelector(`section[id=${link}]`)
-            .scrollIntoView({ block: 'start', behavior: 'smooth' })
-    })
-})
