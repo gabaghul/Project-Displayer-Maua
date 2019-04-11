474f4748bc36c8ab959a84c76856d47596698a94
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-04-10 21:12:20 -0300

    cards fixed

--- src/js/card.js
+++ src/js/card.js
@@ -76,7 +76,7 @@ function animateCoverUp(card) {
     setCoverColor(cardStyle);
     scaleCoverToFillWindow(cardPosition);
     // update the content of the opened page
-    openContentText.innerHTML = '<h1>' + card.children[2].children[0].textContent + '</h1>' + paragraphText + buttonSubmit;
+    openContentText.innerHTML = '<h1 class="overflow-hidden">' + card.children[2].children[0].textContent + '</h1>' + paragraphText + buttonSubmit;
     openContentImage.src = card.children[1].src;
     setTimeout(function () {
         // update the scroll position to 0 (so it is at the top of the 'opened' page)
@@ -182,7 +182,11 @@ function resize() {
     windowHeight = window.innerHeight;
 }
 
-var buttonSubmit = `<button class="btn btn-primary">Interessado? Inscreva-se aqui</button>`
-var paragraphText = `<p class="text-justify col-12">Somebody once told me the world is gonna roll me. I ain\'t the sharpest tool in the shed. She was looking kind of dumb with her finger and her thumb in the shape of an "L" on her forehead. Well the years start coming and they don\'t stop coming. Fed to the rules and I hit the ground running. Didn\'t make sense not to live for fun. Your brain gets smart but your head gets dumb. So much to do, so much to see. So what\'s wrong with taking the back streets? You\'ll never know if you don\'t go. You\'ll never shine if you don\'t glow.</p>
-                     <p class="text-justify col-12">Hey now, you\'re an all-star, get your game on, go play. Hey now, you\'re a rock star, get the show on, get paid. And all that glitters is gold. Only shooting stars break the mold.</p>
-                     <p class="text-justify col-12">It\'s a cool place and they say it gets colder. You\'re bundled up now, wait till you get older. But the meteor men beg to differ. Judging by the hole in the satellite picture. The ice we skate is getting pretty thin. The water\'s getting warm so you might as well swim. My world\'s on fire, how about yours? That\'s the way I like it and I never get bored.</p>`;
+var buttonSubmit = `<p><button class="btn btn-primary float-right">Interessado? Inscreva-se aqui</button><p>`
+var paragraphText = `<p class="text-justify">Somebody once told me the world is gonna roll me. I ain\'t the sharpest tool in the shed. She was looking kind of dumb with her finger and her thumb in the shape of an "L" on her forehead. Well the years start coming and they don\'t stop coming. Fed to the rules and I hit the ground running. Didn\'t make sense not to live for fun. Your brain gets smart but your head gets dumb. So much to do, so much to see. So what\'s wrong with taking the back streets? You\'ll never know if you don\'t go. You\'ll never shine if you don\'t glow.</p>
+                     <p class="text-justify">Hey now, you\'re an all-star, get your game on, go play. Hey now, you\'re a rock star, get the show on, get paid. And all that glitters is gold. Only shooting stars break the mold.</p>
+                     <p class="text-justify">It\'s a cool place and they say it gets colder. You\'re bundled up now, wait till you get older. But the meteor men beg to differ. Judging by the hole in the satellite picture. The ice we skate is getting pretty thin. The water\'s getting warm so you might as well swim. My world\'s on fire, how about yours? That\'s the way I like it and I never get bored.</p>
+                     <p class="h2 my-3">Professores responsáveis:</p>
+                     <p>Cara Bacana 1</p>
+                     <p>Cara Bacana 2</p>
+                     <p>Cara Bacana 3</p>`;
