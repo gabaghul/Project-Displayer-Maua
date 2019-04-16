a20580f55c4959ccdaf2a86c8d289674045799f7
kikepola <kikepola@gmail.com>
2019-04-15 21:54:12 -0300

    api working

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,26 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './componentes/Card'
 
+var itens = [];
 
-
-function load_array(){
-    var json_file = require('./file_data.json');
-    var itens = [];
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var Http = new XMLHttpRequest();
+Http.open('GET', 'http://icapi/api/cards');
+Http.onload = function() {
+    var json_file =JSON.parse(Http.responseText);
+    console.log(Http.responseText)
     var i_count = 0;
-    var counter = json_file["cards"].length - 1
-
-    while(i_count < json_file["cards"].length){
-        var name = json_file["cards"][i_count]["name"]
-        var text1 = json_file["cards"][i_count]["text1"]
-        var text2 = json_file["cards"][i_count]["text2"]
+    var counter = json_file.length;
+    while(i_count < counter){
+        var name = json_file[i_count]["name_card"]
+        var text1 = json_file[i_count]["text1"]
+        var text2 = json_file[i_count]["text2"]
 
         const file= <Card class="col-sm" name={name} key_ref={i_count} text2={text2} text1={text1}/>
         itens.push(file)
     i_count = 1 + i_count
     }
-    return itens
+    ReactDOM.render(itens, document.getElementById('react'))
 }
+Http.send()
 
-if(load_array()){
-    ReactDOM.render(load_array(), document.getElementById('react'))
-}
+
