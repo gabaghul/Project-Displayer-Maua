ee21ef1e7664fe1608d81e78def30aa2673311cb
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-05-16 12:26:37 -0300

    novas section

--- frontend/src/js/scrolling-nav.js
+++ frontend/src/js/scrolling-nav.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
   $('a.js-scroll-trigger').click(e => {
     e.preventDefault()
     const link = e.target.getAttribute('href').substr(1)
-    // console.log(link, typeof link)
+    console.log(link, typeof link)
     document.querySelector(`section[id=${link}]`)
       .scrollIntoView({ block: 'start', behavior: 'smooth' })
   });
