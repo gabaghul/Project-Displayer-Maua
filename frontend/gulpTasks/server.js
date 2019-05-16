e3bde0300fe892d1adac83c4f9d765a19f13ec9f
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-05-16 13:24:00 -0300

    browser sync configurado

--- frontend/gulpTasks/server.js
+++ frontend/gulpTasks/server.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const watch = require('gulp-watch')
-const webserver = require('gulp-webserver')
+// const webserver = require('gulp-webserver')
+const browserSync = require('browser-sync').create();
 
 gulp.task('monitorarMudancas', () => {
     watch('src/**/*.html', () => gulp.start('app.html'))
@@ -9,10 +10,20 @@ gulp.task('monitorarMudancas', () => {
     watch('src/img/**/*.*', () => gulp.start('app.imgs'))
 })
 
+// gulp.task('servidor', ['monitorarMudancas'], () => {
+//     return gulp.src('build').pipe(webserver({
+//         livereload: true,
+//         port: 9000,
+//         open: true
+//     }))
+// })
+
 gulp.task('servidor', ['monitorarMudancas'], () => {
-    return gulp.src('build').pipe(webserver({
-        livereload: true,
-        port: 9000,
-        open: true
-    }))
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        },
+        ui : { port: 9001},
+        port: 9000
+    });
 })
