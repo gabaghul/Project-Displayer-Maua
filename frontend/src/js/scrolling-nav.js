be9dd7ba772e7577fcca05cd9996b738adcb5c1c
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-06-01 12:31:43 -0300

    refatoração

--- frontend/src/js/scrolling-nav.js
+++ frontend/src/js/scrolling-nav.js
@@ -1,20 +1,20 @@
 $(document).ready(() => {
-  $('body').scrollspy({
-    target: '#mainNav'
-  });
-
-  $('a.js-scroll-trigger').click(e => {
-    e.preventDefault()
-    const link = e.target.getAttribute('href').substr(1)
-    // console.log(link, typeof link)
-    document.querySelector(`section[id=${link}]`)
-      .scrollIntoView({ block: 'start', behavior: 'smooth' })
-  });
-
-  $(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $('body').scrollspy({
+        target: '#mainNav'
     });
-  });
+
+    $('a.js-scroll-trigger').click(e => {
+        e.preventDefault()
+        const link = e.target.getAttribute('href').substr(1)
+        // console.log(link, typeof link)
+        document.querySelector(`section[id=${link}]`)
+            .scrollIntoView({ block: 'start', behavior: 'smooth' })
+    });
+
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
 })
